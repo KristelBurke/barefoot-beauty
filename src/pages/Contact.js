c60d163d05d96bfee9067f0388b9245fcd8bda5b
dutchessk <148763839+KristelBurke@users.noreply.github.com>
2024-02-01 14:25:08 +0200

    character count implemented

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,11 @@
-import React,  { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { ToastContainer, toast } from "react-toastify";
 
 function Contact() {
   const form = useRef();
+
+  const [message, setMessage] = useState('');
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -73,23 +75,18 @@ function Contact() {
             <p className="mt-12 max-w-3xl text-md text-brown">Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.</p>
             
             <dl className="mt-12 space-y-6 sm:flex sm:flex-col sm:items-center sm:justify-center lg:items-start lg:justify-start">
-                <dt><span className="sr-only">Phone number</span></dt>
                 <dd className="flex text-md text-brown">
                 <svg className="h-6 w-6 flex-shrink-0 text-gold" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                 </svg>
                   <span className="ml-3">+372 1234 5678</span>
                 </dd>
-                <dt><span className="sr-only">Email</span></dt>
                 <dd className="flex text-md text-brown">
-                <dt><span className="sr-only">Address</span></dt>
                 <svg className="h-6 w-6 flex-shrink-0 text-gold" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                 </svg>
                 <a href="mailto:info@barefootbeauty.com" className="ml-3 underline">info@barefootbeauty.com</a>
                 </dd>
-
-                <dt><span className="sr-only">Address</span></dt>
                 <dd className="flex text-md text-brown">
                 <svg  className="w-6 h-6 flex-shrink-0 text-gold" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -137,12 +134,18 @@ function Contact() {
                   </div>
                 </div>
                 <div className="sm:col-span-2">
-                  <div className="flex justify-between">
-                    <label htmlFor="message" className="text-sm font-medium text-brown flex justify-between ">Message</label>
-                    <span id="message-max" className="text-sm text-gold">Max. 500 characters</span>
+                <div className="flex justify-between">
+                  <label htmlFor="message" className="text-sm font-medium text-brown">Message</label>
+                    <span id="message-max" className="text-sm text-gold">
+                      {message.length}/500
+                    </span>
                   </div>
                   <div className="mt-1">
-                    <textarea id="message" name="message" rows="4" className="block w-full rounded-md border border-gold px-4 py-3 text-brown shadow-sm focus:border-gold focus:ring-gold" aria-describedby="message-max"></textarea>
+                  <textarea 
+                    id="message" name="message" rows="4" maxLength="500" value={message} 
+                    onChange={(e) => setMessage(e.target.value)} className="block w-full rounded-md border border-gold px-4 py-3 text-brown shadow-sm focus:border-gold focus:ring-gold" 
+                    aria-describedby="message-max">
+                    </textarea>
                   </div>
                 </div>
                 <div className="sm:col-span-2 sm:flex sm:justify-end">
